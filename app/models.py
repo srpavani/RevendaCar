3c63b8a522dfb91338f7810ff0d4785eefc95ffb
diogopavani <53492119+srpavani@users.noreply.github.com>
2024-04-19 19:29:14 -0300

    :sparkles: add models car inventory

--- app/models.py
+++ app/models.py
@@ -19,5 +19,21 @@ class CarroModel(models.Model):
     
     def __str__(self):
         return self.model
+    
+    
+class CarInvetory(models.Model):
+    cars_count = models.IntegerField()
+    cars_value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_at'] #ordenando pelo mais atual/recente
+        
+    def __str__(self):
+        return f'{self.cars_count} - {self.cars_value}'   
+    
+     
+     
+        
 
     
