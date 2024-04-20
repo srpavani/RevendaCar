1c7c82dc7bd05a941957add1695b6c23b2f7ebc4
diogopavani <53492119+srpavani@users.noreply.github.com>
2024-04-19 22:42:08 -0300

    finalizando

--- openai_api/client.py
+++ openai_api/client.py
@@ -5,7 +5,7 @@ def get_car_ai_bio(model, brand, year):
     Provide a sales description for the {} {} {} car in 250 characters or less, highlighting its positive features to aid in selling the car. Be specific and concise.
     '''.format(brand, model, year)
     
-    openai.api_key = ''  
+    
     
     response = openai.Completion.create(
         engine="gpt-3.5-turbo",
